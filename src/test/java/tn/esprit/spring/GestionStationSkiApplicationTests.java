81d8b229cbcf37c2789e8f25bc758706d1b43a6e
ahlem-said <ahlem.said@esprit.tn>
2023-11-04 10:31:09 +0100

    Mockito

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,10 +1,7 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 
