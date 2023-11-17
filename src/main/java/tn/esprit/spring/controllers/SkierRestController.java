21911388b163357f32eb2ec2906259b6fe0379e2
Fadhel <61436697+Fadhel-kamel@users.noreply.github.com>
2023-11-17 21:46:41 +0100

    Update SkierRestController.java

--- src/main/java/tn/esprit/spring/controllers/SkierRestController.java
+++ src/main/java/tn/esprit/spring/controllers/SkierRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Tag(name = "\uD83C\uDFC2 Skier Management")
 @RestController
 @RequestMapping("/skier")
-@CrossOrigin(origins = "http://192.168.33.10")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class SkierRestController {
 
